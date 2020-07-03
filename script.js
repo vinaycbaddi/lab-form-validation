02bb38258a967875d1a5fa1cf2aee0018a3e9dd6
root <root@LAPTOP-ODE2T0U9.localdomain>
2020-07-03 20:40:27 +0530

    Vinay(S-55)

--- script.js
+++ script.js
@@ -8,6 +8,38 @@
 // username should not contain special characters
 // the function should return true if all the conditions above are validated.
 // return false if the validation fails
+let button = document.getElementById('isubmit');
+button.addEventListener('click', checkUserName);
+function checkUserName() {
+    checkUser();
+    checkEmail();
+    checkPassword();
+}
+function checkUser() {
+    let user = document.getElementById('iusertext').value;
+    let uerror = document.getElementById('name_error');
+    console.log(user);
+    if (user == null || user == '') {
+        uerror.innerHTML = 'Please enter a valid name'
+        return false;
+    }
+    else if (user.length < 8) {
+        uerror.innerHTML = 'Name length is less than 8 charcters'
+        return false;
+    }
+    else if (user.length > 15) {
+        uerror.innerHTML = 'Name length is more than 15 charcters'
+        return false;
+    }
+    else if (user.match(/\s/g)) {
+        uerror.innerHTML = 'Contains Whitespace.'
+        return false;
+    }
+    else if (/^[a-zA-Z0-9]*$/.test(user) == false) {
+        uerror.innerHTML = 'Contains Special Characters.'
+        return false;
+    }
+}
 
 // Progression 2 
 // Check mail
@@ -16,9 +48,29 @@
 // It can be alphanumeric in nature
 // return true if it passses all validation and false otherwise
 
+function checkEmail() {
+    let email = document.getElementById('iemailtext').value;
+    let emerror = document.getElementById('email_error')
+    var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    ;
+    if (reg.test(email) == false) {
+        emerror.innerHTML = "Invalid Email";
+        return (false);
+    }
+}
+
 // Progression 3
 // Check password
 // create a function called as checkPassword()
 // should be alphanumeric in nature
 // password must contain atleast one Uppercase, one number and special characters[!@#$%^&*()_]
 // return true else return false
+function checkPassword() {
+    var regx = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    let pass = document.getElementById('ipasswordtext').value;
+    let passerror = document.getElementById('password_error')
+    if (!regx.test(pass)) {
+        passerror.innerHTML = 'Invalid Password';
+        return false;
+    }
+}
